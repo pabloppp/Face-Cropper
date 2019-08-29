662ec0b134ec6c7435d9a510c25c2978b85a2369
pablo pernias <pablo.pernias@frontiersin.org>
2019-08-29 15:32:09 +0200

    error handling

--- crop.py
+++ crop.py
@@ -7,6 +7,7 @@ import numpy as np
 padding = 0.3 # percentage of width/height to add padding to
 
 face_detector = dlib.get_frontal_face_detector()
+Image.MAX_IMAGE_PIXELS = 999999999
 
 def extract_padded_features(x):
 	padd = int((x.right() - x.left()) * padding)
@@ -21,15 +22,18 @@ images_processed = 0
 faces_generated = 0
 
 for path in tqdm(glob.glob('./images/*.jpg')):
-	images_processed += 1
-	file_name = path.split('\\')[-1].replace('.jpg', '')
+	try:
+		images_processed += 1
+		file_name = path.split('\\')[-1].replace('.jpg', '')
 
-	image = Image.open(path) # io.imread(path)
-	detected_faces = detect_faces(np.array(image))
+		image = Image.open(path) # io.imread(path)
+		detected_faces = detect_faces(np.array(image))
 
-	for n, face_rect in enumerate(detected_faces):
-		faces_generated += 1
-		face = image.crop(face_rect).resize((512, 512))
-		face.save(f'./output/{file_name}_f{n+1}.jpg')
+		for n, face_rect in enumerate(detected_faces):
+			faces_generated += 1
+			face = image.crop(face_rect).resize((512, 512))
+			face.save(f'./output/{file_name}_f{n+1}.jpg')
+	except: 
+		print(f"An exception occurred with image {path}")
 
 print(f"Successfully generated {faces_generated} faces from {images_processed} images!!!")
